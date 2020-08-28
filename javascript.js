836d383a353060488e6502be8d55ee48d9baec35
therealtomisin <oluwatimilehineribake@gmail.com>
2020-08-28 21:53:51 +1000

    starting again

--- javascript.js
+++ javascript.js
@@ -1,24 +1,42 @@
 let login = () =>{
     form = document.forms["form-login"]
     console.log(form)
-    form.addEventListener("submit", (e)=>{
+    let button = form.querySelector('.button');
+    button.addEventListener("click", (e)=>{
         e.preventDefault()
+        let email =  form.querySelector('.email').value
         let username = form.querySelector('.username').value
         let password = form.querySelector('.password').value
-        // console.log(username)
-        // console.log(password)
-        let newusername = 
-        
-        class user  {
-            constructor(theusername, thepassword){
-                this.username = theusername;
-                this.password = thepassword;
+
+        let emailRegex = /^([a-z\d\.-]+)@([a-z]+)\.([a-z]{2,5})(\.[a-z]{2,5})?$/ig;
+        let usernameRegex = /^[a-z0-9]{5,20}$/gi
+        let passwordRegex = /^[a-z0-9]{6,12}$/gi
+
+        console.log(emailRegex.test(email))
+        console.log(usernameRegex.test(username))
+        console.log(passwordRegex.test(password))
+
+        if (emailRegex.test(email) ){
+            if (passwordRegex.test(password) ){
+                class user  {
+                    constructor(theEmail, theusername, thepassword){
+                        this.email = theEmail;
+                        this.username = theusername;
+                        this.password = thepassword;
+                    }
+                }
+                let user1 = new user(email, username, password)
+                console.log(user1)
+                form.querySelector('.email').value = ''
+                form.querySelector('.username').value = ''
+                form.querySelector('.password').value = ''
             }
         }
-        let user1 = new user(username, password)
-       // let user2 = new user ("Balkindo", "12345")
-        console.log(user1)
-        console.log(user2)
+                else {
+                    return console.error('invalid username or password');
+                    
+        }
+       
     })
 }
 login()
