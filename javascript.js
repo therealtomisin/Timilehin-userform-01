a5d59a244f7c7f7554b69a02126d5e202d5630a1
therealtomisin <oluwatimilehineribake@gmail.com>
2020-08-28 21:54:11 +1000

    moving on

--- javascript.js
+++ javascript.js
@@ -1,45 +1,42 @@
-let login = () =>{
-    form = document.forms["form-login"]
-    console.log(form)
-    let button = form.querySelector('.button');
-    button.addEventListener("click", (e)=>{
-        e.preventDefault()
-        let email =  form.querySelector('.email').value
-        let username = form.querySelector('.username').value
-        let password = form.querySelector('.password').value
+let inputs = document.querySelectorAll("input")
 
-        let emailRegex = /^([a-z\d\.-]+)@([a-z]+)\.([a-z]{2,5})(\.[a-z]{2,5})?$/ig;
-        let usernameRegex = /^[a-z0-9]{5,20}$/gi
-        let passwordRegex = /^[a-z0-9]{6,12}$/gi
-
-        console.log(emailRegex.test(email))
-        console.log(usernameRegex.test(username))
-        console.log(passwordRegex.test(password))
-
-        if (emailRegex.test(email) ){
-            if (passwordRegex.test(password) ){
-                class user  {
-                    constructor(theEmail, theusername, thepassword){
-                        this.email = theEmail;
-                        this.username = theusername;
-                        this.password = thepassword;
-                    }
-                }
-                let user1 = new user(email, username, password)
-                console.log(user1)
-                form.querySelector('.email').value = ''
-                form.querySelector('.username').value = ''
-                form.querySelector('.password').value = ''
-            }
-        }
-                else {
-                    return console.error('invalid username or password');
-                    
-        }
-       
-    })
+let regexValues = {
+    email: /^([a-z\d\.-]+)@([a-z]+)\.([a-z]{2,5})(\.[a-z]{2,5})?$/ig,
+    username: /^[a-z0-9]{5,20}$/gi,
+    password: /^[a-z0-9]{6,12}$/gi
 }
-login()
+
+    validateRegex=(field, value)=>{
+        if (field.value = value){
+            class user  {
+                constructor(theEmail, theusername, thepassword){
+                    this.email = theEmail;
+                    this.username = theusername;
+                    this.password = thepassword;
+                    }
+                 }
+                 let user1 = new user(email, username, password)
+                 console.log(user1)
+        }
+        else{
+            return console.error('invalid username or password');
+        }
+    }
+
+    //     if (emailRegex.test(email) && usernameRegex.test(username)){
+    //         if (username.length < 20 && passwordRegex.test(password)){
+    //             
+    //         }
+    //     }
+    //             else {
+    //                 
+                    
+    //     }
+    //     form.querySelector('.email').value = ''
+    //     form.querySelector('.username').value = ''
+    //     form.querySelector('.password').value = ''
+    // })
+
 
 
 
@@ -59,3 +56,13 @@ login()
     name: "Timi"
 
 } */
+
+// let button = form.querySelector('.button');
+// button.addEventListener("click", (e)=>{
+//     e.preventDefault()
+//     let email =  form.querySelector('.email').value
+//     let username = form.querySelector('.username').value
+//     let password = form.querySelector('.password').value
+
+
+// 
